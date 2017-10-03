306035d75a18c4f93c3ef64d049fdb509499fd87
Rushabh <rushabhshah341@gmail.com>
2017-10-03 11:19:14 -0400

    Changed Session Seceret

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 /**
  * Module dependencies.
  */
-process.env.SESSION_SECRET = "PropertyPortal";
+process.env.SESSION_SECRET = 'YourHousing';
 process.env.NODE_ENV = 'development';
 var app = require('./config/lib/app');
 var server = app.start();
